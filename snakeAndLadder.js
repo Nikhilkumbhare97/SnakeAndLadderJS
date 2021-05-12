2778cca02aacf9bb4bdcf4e999393ad637a44a29
nikhilkumbhare <nikhilk190697@gmail.com>
2021-05-12 20:16:54 +0530

    Added: Ability to roll a die and get Number

--- snakeAndLadder.js
+++ snakeAndLadder.js
@@ -2,3 +2,9 @@ console.log("Welcome To Snake And Ladder Game");
 
 const START_POSITION = 0;
 const END_POSITION = 100;
+
+rollDie = () => {
+    let diceNumber = (Math.floor(Math.random() * 10) % 6) + 1;
+    console.log("Dice Number : " + diceNumber);
+}
+rollDie();
