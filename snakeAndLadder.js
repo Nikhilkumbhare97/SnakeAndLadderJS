e7e8bcaadbaa51b57d597573bfdc08cc998a8e11
nikhilkumbhare <nikhilk190697@gmail.com>
2021-05-12 21:02:37 +0530

    Added: To Ensure Player gets Exact Winning Position 100

--- snakeAndLadder.js
+++ snakeAndLadder.js
@@ -10,22 +10,27 @@ var currentPosition = START_POSITION;
 
 rollDie = () => {
     let diceNumber = (Math.floor(Math.random() * 10) % 6) + 1;
-    console.log("Dice Number : " + diceNumber);
     return diceNumber;
 }
 
 option = () => {
     let checkOption = Math.floor(Math.random() * 10) % 3;
+    let die = rollDie();
     switch (checkOption) {
         case LADDER:
-            currentPosition += rollDie();
-            console.log("Player got Ladder and it Move to position : " + currentPosition);
-            break;
+            if ((currentPosition + die) > END_POSITION) {
+                console.log("Out of Reach")
+                currentPosition += 0;
+                break;
+            } else {
+                currentPosition += die;
+                console.log("Player got Ladder and it jump by "+ die +" position and it Move to position : " + currentPosition);
+                break;
+            }
         case SNAKE:
-            let die = rollDie();
             if ((currentPosition - die) >= START_POSITION) {
                 currentPosition -= die;
-                console.log("Player got Snake and it Move to position : " + currentPosition);
+                console.log("Player got Snake and it downs by " + die + " position and it Move to position : " + currentPosition);
                 break;
             } else {
                 currentPosition = START_POSITION;
@@ -37,12 +42,15 @@ option = () => {
             console.log("Player got No Play and it on position : " + currentPosition);
             break;
     }
+    return currentPosition;
 }
 
 winPosition = () => {
     while (currentPosition < END_POSITION) {
-        option();
+        var lastPosition = option();
+        if (lastPosition == END_POSITION){
+            console.log("Player Reach the Winning Position And Won the Game");
+        }
     }
 }
-
 winPosition();
